1295bc1f8f9aea5d89bcd7faf567daeb53235bdd
Antti Linno <antti.linno@gmail.com>
2024-05-27 14:40:47 +0000

    Format all config files and add stylua config

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -1,10 +1,9 @@
 return {
   "ellisonleao/gruvbox.nvim",
   name = "gruvbox",
-  priority = 1000 ,
+  priority = 1000,
   config = function()
     vim.o.background = "dark"
-    vim.cmd.colorscheme "gruvbox"
-  end
+    vim.cmd.colorscheme("gruvbox")
+  end,
 }
-
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -2,37 +2,39 @@ return {
   {
     "williamboman/mason.nvim",
     config = function()
-      require("mason").setup()
-    end
+      require("mason").setup({
+        ensure_installed = { "black", "isort" },
+      })
+    end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "tsserver", "pyright" },
+        ensure_installed = { "bashls", "lua_ls", "tsserver", "pyright" },
       })
-    end
+    end,
   },
   {
     "neovim/nvim-lspconfig",
     config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-
-      local lspconfig = require('lspconfig')
+      local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({
         capabilities = capabilities,
         settings = {
           Lua = {
             diagnostics = {
-              globals = {'vim'}
-            }
-          }
-        }
+              globals = { "vim" },
+            },
+          },
+        },
       })
       lspconfig.tsserver.setup({})
       lspconfig.pyright.setup({})
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-    end
+      lspconfig.bashls.setup({})
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+    end,
   },
 }
