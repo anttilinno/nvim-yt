5cf3227b6d70bcb0e39cee7e4a483df5142671c2
Antti <antti.linno@gmail.com>
2024-06-14 23:29:12 +0300

    Configure toggleterm for lazygit

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -2,3 +2,5 @@
 vim.keymap.set("n", ";", ":", { desc = "CMD enter command mode" })
 -- Save with Ctrl+s
 vim.keymap.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
+-- Open terminal
+vim.keymap.set({ "n" }, "<C-Space>", "<cmd>ToggleTerm direction=float<cr>", { desc = "Floating terminal" })
