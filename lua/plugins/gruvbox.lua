fff7dfb54a7da662adbe774a98c19b4266b23857
Antti <antti.linno@gmail.com>
2024-06-24 17:23:38 +0300

    Remove auto setting of gruvbox theme

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -2,8 +2,5 @@ return {
   "ellisonleao/gruvbox.nvim",
   name = "gruvbox",
   priority = 1000,
-  config = function()
-    vim.o.background = "dark"
-    vim.cmd.colorscheme("gruvbox")
-  end,
+  config = true,
 }
