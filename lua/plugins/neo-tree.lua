e4f7a5b95ab5048225730e87b5c4a5bdf3a9d298
Antti <antti.linno@gmail.com>
2024-06-24 15:20:55 +0300

    Make neo tree toggable with same key

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,7 +6,44 @@ return {
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
   },
-  config = function()
-    vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<cr>", {})
+  config = true,
+  keys = function()
+    local find_buffer_by_type = function(type)
+      for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+        local ft = vim.api.nvim_buf_get_option(buf, "filetype")
+        if ft == type then
+          return buf
+        end
+      end
+      return -1
+    end
+    local toggle_neotree = function(toggle_command)
+      if find_buffer_by_type("neo-tree") > 0 then
+        require("neo-tree.command").execute({ action = "close" })
+      else
+        toggle_command()
+      end
+    end
+
+    return {
+      {
+        "<A-n>",
+        function()
+          toggle_neotree(function()
+            require("neo-tree.command").execute({ action = "focus", reveal = true, dir = vim.uv.cwd() })
+          end)
+        end,
+        desc = "Toggle Explorer (cwd)",
+      },
+      {
+        "<C-n>",
+        function()
+          toggle_neotree(function()
+            require("neo-tree.command").execute({ action = "focus", reveal = true })
+          end)
+        end,
+        desc = "Toggle Explorer (root)",
+      },
+    }
   end,
 }
